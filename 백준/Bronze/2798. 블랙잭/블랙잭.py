88d43fdfce42c543600d6c774d15fa83342e6af3
Sojeong Kwon <122436557+nachocatee@users.noreply.github.com>
2023-02-26 03:07:53 +0900

    [Bronze II] Title: 블랙잭, Time: 68 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -0,0 +1,10 @@
+N, M = map(int, input().split())
+lst = list(map(int, input().split()))
+
+ans = 0
+for i in range(N-2):
+    for j in range(i+1, N-1):
+        for k in range(j+1, N):
+            if ans < lst[i] + lst[j] + lst[k] <= M:
+                ans = lst[i] + lst[j] + lst[k]
+print(ans)
