d3aa7639aa276adb6b13e5daf23ef3d88b9f6f0f
Sojeong Kwon <122436557+nachocatee@users.noreply.github.com>
2023-03-04 15:20:25 +0900

    [D3] Title: 정곤이의 단조 증가하는 수, Time: 2,288 ms, Memory: 153,616 KB -BaekjoonHub

--- SWEA/D3/6190. 정곤이의 단조 증가하는 수/정곤이의 단조 증가하는 수.py
+++ SWEA/D3/6190. 정곤이의 단조 증가하는 수/정곤이의 단조 증가하는 수.py
@@ -1,22 +1,24 @@
-t = int(input())
-
-def danjo(n):
-    n = str(n)
-    for i in range(len(n)-1):
-        if n[i] > n[i+1]:
-            return 0
-    return 1
-
-for tc in range(1,t+1):
-    n = int(input())
-    a = list(map(int,input().split()))
-    maxn = 0
-    for i in range(n-1):
-        for j in range(i+1,n):
-            num = a[i]*a[j]
-            if danjo(num):
-                maxn = max(num,maxn)
-        if maxn == 0:
-            maxn = -1
-
-    print('#%d %d' % (tc,maxn))
+T = int(input())
+for t in range(1, T+1):
+    N = int(input())
+    arr = list(map(int, input().split()))
+    num_lst = []
+    for i in range(N-1):
+        for j in range(i, N):
+            if i != j:
+                num_lst.append(arr[i] * arr[j])
+    res = [-1]
+    for i in num_lst:
+        a = i
+        tmp = []
+        cnt = 0
+        while a > 0:
+            tmp.append(a % 10)
+            a //= 10
+        # print(tmp)
+        for j in range(len(tmp)-1):
+            if tmp[j] >= tmp[j+1]:
+                cnt += 1
+        if cnt == len(tmp)-1:
+            res.append(i)
+    print(f'#{t} {max(res)}')
