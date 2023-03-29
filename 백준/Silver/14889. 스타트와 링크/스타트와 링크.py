7c909e3950c1928323d1c854e170b071f810b8fe
Sojeong Kwon <122436557+nachocatee@users.noreply.github.com>
2023-03-29 22:25:58 +0900

    [Silver II] Title: 스타트와 링크, Time: 1428 ms, Memory: 122344 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -1,28 +1,34 @@
-def dfs(depth, idx):
-    global min_diff
-    if depth == n//2:
-        power1, power2 = 0, 0
-        for i in range(n):
-            for j in range(n):
-                if visited[i] and visited[j]:
-                    power1 += graph[i][j]
-                elif not visited[i] and not visited[j]:
-                    power2 += graph[i][j]
-        min_diff = min(min_diff, abs(power1-power2))
-        return
+import sys
+input = sys.stdin.readline
 
-    for i in range(idx, n):
+
+def backtracking(depth, n):
+    global min_
+    if depth == N//2:
+        team1 = 0
+        team2 = 0
+        for i in range(N):
+            for j in range(N):
+                if visited[i] and visited[j]:
+                    team1 += board[i][j]
+                elif not visited[i] and not visited[j]:
+                    team2 += board[i][j]
+        min_ = min(min_, abs(team1-team2))
+        return
+    for i in range(n, N):
         if not visited[i]:
             visited[i] = True
-            dfs(depth+1, i+1)
+            backtracking(depth+1, i+1)
             visited[i] = False
 
 
-n = int(input())
-
-visited = [False for _ in range(n)]
-graph = [list(map(int, input().split())) for _ in range(n)]
-min_diff = int(1e9)
-
-dfs(0, 0)
-print(min_diff)
+N = int(input())
+board = [list(map(int, input().split())) for _ in range(N)]
+visited = [False] * N
+# player = list(range(N))
+# for i in board:
+#     print(i)
+# tmp = []
+min_ = 100
+backtracking(0, 0)
+print(min_)
