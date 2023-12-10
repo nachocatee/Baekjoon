ddd311a8e9a021bdc8a26d465e0f55fa5f254bf0
권소정 (Sojeong Kwon) <122436557+nachocatee@users.noreply.github.com>
2023-12-10 17:18:53 +0900

    [Bronze II] Title: 분해합, Time: 2584 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -0,0 +1,16 @@
+N = input()
+res_list = []
+num = '1'
+while int(num) < int(N):
+    tmp = int(num)
+    for i in num:
+        tmp += int(i)
+    if tmp == int(N):
+        res_list.append(int(num))
+        break
+    else:
+        num = str(int(num) + 1)
+if res_list:
+    print(max(res_list))
+else:
+    print(0)
