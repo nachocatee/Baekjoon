7d342b2f23410d1f66eef9f07cb2a49ee1980cb5
권소정 (Sojeong Kwon) <122436557+nachocatee@users.noreply.github.com>
2023-12-11 20:07:35 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 4440 ms, Memory: 53948 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.py
@@ -0,0 +1,10 @@
+N = int(input())
+lst = []
+for _ in range(N):
+    x, y = map(int, input().split())
+    lst.append([x, y])
+lst.sort(key=lambda x: (x[1], x[0]))
+for i in lst:
+    for j in i:
+        print(j, end=" ")
+    print()
